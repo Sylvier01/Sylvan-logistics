916dbd9354c4f3d95b33b22539c21691162d531f
Sylvier01 <106278340+Sylvier01@users.noreply.github.com>
2026-09-22 09:58:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ if(cartIsEmpty && clearCart){
 }
     
 cartContainer.addEventListener("click", () =>{
-    window.open("cart.html")
+    window.location.href = "cart.html"
 })
 
 //Total cart amount
@@ -577,7 +577,7 @@ products.forEach((product) =>{
         localStorage.setItem("productName", productName)
         localStorage.setItem("productPrice", productPrice)
         
-        window.open("product.html")
+        window.location.href = "product.html"
     })
 })
 
